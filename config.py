d02af2770b3f7a860fec53c999cfa0e317df0975
zhanggong <601806353@qq.com>
2020-10-20 12:33:16 +0800

    修改batchsize

---
+++ config.py
@@ -0,0 +1,11 @@
+train_path = './data'
+anchors = [
+    [(116,90),(156,198),(373,326)],
+    [(30,61),(62,45),(59,119)],
+    [(10,13),(16,30),(33,23)]
+]
+image_size= 416
+classes = 4
+batch_size =8
+resume = False
+epoch = 50
